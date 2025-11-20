fbfd78464cdde606aedac02a61afa0230f1b43f8
shadowxmd2-cyber <shadowxmd2@gmail.com>
2025-11-20 21:28:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "HtpS2R6I#QGWjTCFxWIEu6mvUpU601-iZzxg23i4KGQ2MsPsz6sk",
+  MONGODB: process.env.MONGODB || "mongodb+srv://shadowxmd2_db_user:f2MowJLaDfSbUHYe@cluster0.dozzrxd.mongodb.net/?appName=Cluster0",
+  OWNER_NUM: process.env.OWNER_NUM || "94764040298",
 };
